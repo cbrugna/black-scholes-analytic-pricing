b1360ac7ecb0299784d7c7b7540361ab452f943f
Casey Brugna <cbrugna1@binghamton.edu>
2021-10-02 01:14:19 -0400

    added functions and makefile

--- main.cpp
+++ main.cpp
@@ -9,6 +9,55 @@ double norm_pdf(const double& x) {
     return (1.0/(pow(2*M_PI,0.5)))*exp(-0.5*x*x);
 }
 
+// Approximate for the CDF of the SND (recurisve) 
+double norm_cdf(const double& x) {
+    double k = 1.0/(1.0 + 0.2316419*x);
+    double k_sum = k*(0.319381530 + k*(-0.356563782 + k*(1.781477937 + k*(-1.821255978 + 1.330274429*k))));
+
+    if (x >= 0.0) {
+        return (1.0 - (1.0/(pow(2*M_PI,0.5)))*exp(-0.5*x*x) * k_sum);
+    } else {
+        return 1.0 - norm_cdf(-x);
+    }
+}
+// Calculate d_j, for j in {1, 2}
+// Appears in function for european call/put functions
+double d_j(const int& j, const double& S, const double& K, const double& r, const double& v, const double& T) {
+    return (log(S/K) + (r + (pow(-1,j-1))*0.5*v*v)*T)/(v*(pow(T,0.5)));
+}
+
+// Call price
+// Based on: S, K, r, sigma(v), T
+double call_price(const double& S, const double& K, const double& r, const double& v, const double& T) {
+    return S * norm_cdf(d_j(1, S, K, r, v, T))-K*exp(-r*T) * norm_cdf(d_j(2, S, K, r, v, T));
+}
+
+// Put price 
+double put_price(const double& S, const double& K, const double& r, const double& v, const double& T) {
+    return -S*norm_cdf(-d_j(1, S, K, r, v, T))+K*exp(-r*T) * norm_cdf(-d_j(2, S, K, r, v, T));
+}
+
 int main() {
-    std::cout << "Hello, world" << std::endl;
+    // Creating paramter list (for now)
+    double S = 100.0;  // Option price
+    double K = 100.0;  // Strike price
+    double r = 0.05;   // Risk-free rate (5%)
+    double v = 0.2;    // Volatility of the underlying (20%)
+    double T = 1.0;    // One year until expiry
+
+    // Using call/put functions
+    double call = call_price(S, K, r, v, T);
+    double put = put_price(S, K, r, v, T);
+
+    //output
+    std::cout << "Underlying:      " << S << std::endl;
+    std::cout << "Strike:          " << K << std::endl;
+    std::cout << "Risk-Free Rate:  " << r << std::endl;
+    std::cout << "Volatility:      " << v << std::endl;
+    std::cout << "Maturity:        " << T << std::endl;
+
+    std::cout << "Call Price:      " << call << std::endl;
+    std::cout << "Put Price:       " << put << std::endl;
+
+    return 0;
 }
